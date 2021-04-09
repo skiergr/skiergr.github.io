986da3e7284e7a29188e4b0e400803090905b3a5
Grady <gradyramberg@gmail.com>
2021-04-09 16:02:27 -0700

    RPS

--- Rock Paper and Scissors/website.js
+++ Rock Paper and Scissors/website.js
@@ -45,8 +45,8 @@ const game = () => {
                 const computerChoice = computerOptions[computerNumber];
                 setTimeout(() => {
                     compareHands(this.textContent, computerChoice);
-                    playerHand.src = `./assets/${this.textContent}.png`;
-                    computerHand.src = `./assets/${computerChoice}.png`;
+                    playerHand.src = `./Assets/${this.textContent}.png`;
+                    computerHand.src = `./Assets/${computerChoice}.png`;
                 }, 2000)
                 playerHand.style.animation = "shakePlayer 2s ease";
                 computerHand.style.animation = "shakeComputer 2s ease";
